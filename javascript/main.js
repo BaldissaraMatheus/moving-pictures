ae94ced5d94e5ecda07ac3d99e79e37b0886a2b3
BaldissaraMatheus <baldissaramatheus@gmail.com>
2018-06-05 13:06:04 -0300

    Add search system

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,20 @@
 $(document).ready(() => {
-  /*
+  
   $('#searchForm').on('submit', (e) => {
     let searchText = console.log($('#searchText').val());
     
+    clearCards();
     getMovies(searchText);
     e.preventDefault();
   });
-  */
+  
   let searchResult = 'raw';
 
   $.ajax({
     method: 'GET',
     url: `http://www.omdbapi.com/?t=${searchResult}&apikey=${key}`,
     dataType: 'json'
+
   }).done((data) => {   
     const title = data.Title;
     const plot = data.Plot;
@@ -20,23 +22,35 @@ $(document).ready(() => {
 
     $.get('includes/card.inc.html', (data) => {
       $('#result').append(data);
-      $('#card-title').text(title).removeAttr('id');
-      $('#card-content').text(plot).removeAttr('id');
+      $('#card-content').text(title).removeAttr('id');
       $('#card-image').attr('src', poster).removeAttr('id');
      });
-    /*
-    $.each(data, (movie, i) => {
-      console.log(movie.Title);
-      $.get('includes/card.inc.html', (data) => {
-       $('#result').append(data);
-       $('#card-title').text(movie.Title).removeAttr('id');
-       //$('#card-content').text(movie.Year).removeAttr('id');
-      });
-    });  
-  */   
   });
 });
 
-function getMovies(text) {
+function getMovies(input) {
+  $.ajax({
+    method: 'GET',
+    url: `http://www.omdbapi.com/?s=${input}&page=1&apikey=${key}`,
+    dataType: 'json'
+    
+  }).done((data) => {
+    console.log(data);
+    $.each(data.Search, (i) => { 
+      const movie = data.Search[i];
+      
+      $.get('includes/card.inc.html', (html) => {
+       $('#result').append(html);
+       $('#card-content').text(movie.Title).removeAttr('id');
+       $('#card-image').attr('src', movie.Poster).removeAttr('id');
+      });
+      
+    });  
+  }).fail(()=> {
+    console.log('erro');
+  });
+}
 
+function clearCards() {
+  $('#result').html('');
 }
